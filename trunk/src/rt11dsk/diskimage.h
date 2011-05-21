53bb48f44d72978a8fd8c141e593966eb13aca9c
nzeemin@gmail.com <nzeemin@gmail.com@d259c8a2-7647-0410-94c4-d9ba3b64c92c>
2011-05-21 16:24:24 +0000

    [*] rt11dsk: some more refactoring.

--- trunk/src/rt11dsk/diskimage.h
+++ trunk/src/rt11dsk/diskimage.h
@@ -1,5 +1,9 @@
 // diskimage.h : Disk image utilities headers
 
+struct CCachedBlock;
+struct CVolumeInformation;
+struct CVolumeCatalogSegment;
+
 //////////////////////////////////////////////////////////////////////
 
 /* Size of each RT-11 disk block, 512 or 0x200 bytes */
@@ -7,8 +11,30 @@
 
 #define NETRT11_IMAGE_HEADER_SIZE  256
 
-struct CCachedBlock;
-struct CVolumeCatalogSegment;
+
+//////////////////////////////////////////////////////////////////////
+// Структура для хранения информации о томе
+struct CVolumeInformation
+{
+    char volumeid[13];
+    char ownername[13];
+    char systemid[13];
+    WORD firstcatalogblock;
+    WORD systemversion;
+    WORD catalogextrawords;
+    WORD catalogentrylength;
+    WORD catalogentriespersegment;
+    WORD catalogsegmentcount;
+    WORD lastopenedsegment;
+    // Массив сегментов
+    CVolumeCatalogSegment* catalogsegments;
+    WORD catalogentriescount;  // Количество валидных записей каталога, включая завершающую ENDMARK
+
+public:
+    CVolumeInformation();
+    ~CVolumeInformation();
+};
+
 
 //////////////////////////////////////////////////////////////////////
 // Образ диска в формате .dsk либо .rtd
@@ -22,6 +48,7 @@ protected:
     int             m_nTotalBlocks;  // Total blocks in the image
     int             m_nCacheBlocks;  // Cache size in blocks
     CCachedBlock*   m_pCache;
+    CVolumeInformation m_volumeinfo;
 
 public:
     CDiskImage();
@@ -50,72 +77,6 @@ public:
 private:
     long GetBlockOffset(int nBlock) const;
 
-};
-
-//////////////////////////////////////////////////////////////////////
-// Структуры данных, представляющие информацию о томе RT-11
-
-/* Types for rtFileEntry 'status' */
-#define RT11_STATUS_TENTATIVE   256     /* Temporary file */
-#define RT11_STATUS_EMPTY       512     /* Marks empty space */
-#define RT11_STATUS_PERM        1024    /* A "real" file */
-#define RT11_STATUS_ENDMARK     2048    /* Marks the end of file entries */
-
-struct CVolumeCatalogEntry;
-struct CVolumeCatalogSegment;
-
-// Структура для хранения информации о томе
-struct CVolumeInformation
-{
-    char volumeid[13];
-    char ownername[13];
-    char systemid[13];
-    WORD firstcatalogblock;
-    WORD systemversion;
-    WORD catalogextrawords;
-    WORD catalogentrylength;
-    WORD catalogentriespersegment;
-    WORD catalogsegmentcount;
-    WORD lastopenedsegment;
-    // Массив сегментов
-    CVolumeCatalogSegment* catalogsegments;
-    WORD catalogentriescount;  // Количество валидных записей каталога, включая завершающую ENDMARK
-
-public:
-    CVolumeInformation();
-    ~CVolumeInformation();
-};
-
-// Структура данных для сегмента каталога
-struct CVolumeCatalogSegment
-{
-public:
-    WORD segmentblock;  // Блок на диске, в котором расположен этот сегмент каталога
-    WORD entriesused;   // Количество использованых записей каталога
-public:
-    WORD nextsegment;   // Номер следующего сегмента
-    WORD start;         // Номер блока, с которого начинаются файлы этого сегмента
-    // Массив записей каталога, размером в максимально возможное кол-во записей для этого сегмента
-    CVolumeCatalogEntry* catalogentries;
-};
-
-// Структура для хранения разобранной строки каталога
-struct CVolumeCatalogEntry
-{
-public:  // Упакованные поля записи
-    WORD status;    // See RT11_STATUS_xxx constants
-    WORD datepac;   // Упакованное поле даты
-    WORD start;     // File start block number
-    WORD length;    // File length in 512-byte blocks
-public:  // Распакованные поля записи
-    TCHAR name[7];  // File name - 6 characters
-    TCHAR ext[4];   // File extension - 3 characters
-
-public:
-    CVolumeCatalogEntry();
-    void Unpack(WORD const * pSrc, WORD filestartblock);  // Распаковка записи из каталога
-    void Pack(WORD* pDest);   // Упаковка записи в каталог
-    void Print();  // Печать строки каталога на консоль
 };
 
 
