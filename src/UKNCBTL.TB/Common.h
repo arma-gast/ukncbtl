32a6e0f3b8f5c622c30a024f52278ad0f7e1d61d
nzeemin@gmail.com <nzeemin@gmail.com@d259c8a2-7647-0410-94c4-d9ba3b64c92c>
2011-11-05 15:33:59 +0000

    [*] UKNCBTL Test Bench.

---
+++ src/UKNCBTL.TB/Common.h
@@ -0,0 +1,76 @@
+/*  This file is part of UKNCBTL.
+    UKNCBTL is free software: you can redistribute it and/or modify it under the terms
+of the GNU Lesser General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+    UKNCBTL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License along with
+UKNCBTL. If not, see <http://www.gnu.org/licenses/>. */
+
+// Common.h
+
+#pragma once
+
+
+//////////////////////////////////////////////////////////////////////
+// Assertions checking - MFC-like ASSERT macro
+
+#ifdef _DEBUG
+
+BOOL AssertFailedLine(LPCSTR lpszFileName, int nLine);
+#define ASSERT(f)          (void) ((f) || !AssertFailedLine(__FILE__, __LINE__) || (DebugBreak(), 0))
+#define VERIFY(f)          ASSERT(f)
+
+#else   // _DEBUG
+
+#define ASSERT(f)          ((void)0)
+#define VERIFY(f)          ((void)f)
+
+#endif // !_DEBUG
+
+
+//////////////////////////////////////////////////////////////////////
+// Alerts
+
+void AlertWarning(LPCTSTR sMessage);
+void AlertWarningFormat(LPCTSTR sFormat, ...);
+
+
+//////////////////////////////////////////////////////////////////////
+// DebugPrint
+
+#if !defined(PRODUCT)
+
+void DebugPrint(LPCTSTR message);
+void DebugPrintFormat(LPCTSTR pszFormat, ...);
+void DebugLog(LPCTSTR message);
+void DebugLogFormat(LPCTSTR pszFormat, ...);
+
+#endif // !defined(PRODUCT)
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+// Processor register names
+const TCHAR* REGISTER_NAME[];
+
+void PrintOctalValue(TCHAR* buffer, WORD value);
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+void Test_Log(char eventtype, LPCTSTR message);
+void Test_LogFormat(char eventtype, LPCTSTR format, ...);
+inline void Test_LogInfo(LPCTSTR message) { Test_Log('i', message); }
+inline void Test_LogError(LPCTSTR message) { Test_Log('E', message); }
+
+void Test_SaveScreenshot(LPCTSTR sFileName);
+void Test_CheckScreenshot(LPCTSTR sFileName);
+
+void Test_CopyFile(LPCTSTR sFileNameFrom, LPCTSTR sFileNameTo);
+
+
+//////////////////////////////////////////////////////////////////////
---
+++ src/UKNCBTL.TB/Emulator.h
@@ -0,0 +1,52 @@
+/*  This file is part of UKNCBTL.
+    UKNCBTL is free software: you can redistribute it and/or modify it under the terms
+of the GNU Lesser General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+    UKNCBTL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License along with
+UKNCBTL. If not, see <http://www.gnu.org/licenses/>. */
+
+// Emulator.h
+
+#pragma once
+
+#include "emubase\Board.h"
+
+//////////////////////////////////////////////////////////////////////
+
+
+extern CMotherboard* g_pBoard;
+
+extern BOOL g_okEmulatorRunning;
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+BOOL Emulator_Init();
+void Emulator_Done();
+void Emulator_SetCPUBreakpoint(WORD address);
+void Emulator_SetPPUBreakpoint(WORD address);
+BOOL Emulator_IsBreakpoint();
+void Emulator_Start();
+void Emulator_Stop();
+void Emulator_Reset();
+int  Emulator_SystemFrame();
+
+void Emulator_PrepareScreenRGB32(void* pBits, const DWORD* colors);
+
+BOOL Emulator_LoadROMCartridge(int slot, LPCTSTR sFilePath);
+BOOL Emulator_AttachFloppyImage(int slot, LPCTSTR sFilePath);
+
+BOOL Emulator_Run(int frames);
+BOOL Emulator_SaveScreenshot(LPCTSTR sFileName);
+int  Emulator_CheckScreenshot(LPCTSTR sFileName);
+void Emulator_KeyboardPressRelease(BYTE ukncscan);
+void Emulator_KeyboardPressReleaseChar(char ch);
+void Emulator_KeyboardSequence(const char * str);
+void Emulator_KeyboardPressReleaseShift(BYTE ukncscan);
+
+
+//////////////////////////////////////////////////////////////////////
