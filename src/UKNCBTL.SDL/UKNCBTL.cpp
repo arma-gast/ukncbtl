4686ae3f60640c33fe4299a1d1300ae917e26531
nzeemin <nzeemin@d259c8a2-7647-0410-94c4-d9ba3b64c92c>
2012-08-23 08:33:08 +0000

    [+] UKNCBTL.SDL: settings in INI file, key events, key mapping for some keys.

--- src/UKNCBTL.SDL/UKNCBTL.cpp
+++ src/UKNCBTL.SDL/UKNCBTL.cpp
@@ -10,6 +10,8 @@
 /////////////////////////////////////////////////////////////////////////////
 // Globals
 
+TSTRING         g_ApplicationExePath;
+TSTRING         g_ApplicationIniPath;
 SDL_Surface*    g_Screen = NULL;
 SDL_Surface*    g_UkncScreen = NULL;
 int             g_UkncScreenWid = UKNC_SCREEN_WIDTH, g_UkncScreenHei = UKNC_SCREEN_HEIGHT;
@@ -18,6 +20,96 @@ int             g_LastDelay = 0;        //DEBUG: Last delay value, milliseconds
 int             g_LastFps = 0;          //DEBUG: Last Frames-per-Second value
 
 #define FRAME_TICKS             40      // 1000 us / 40 = 25 frames per second
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+enum EventType
+{
+    EVKEY = 1,
+    EVJOY = 2,
+    EVCMD = 3,
+};
+
+struct KeyMappingStruct
+{
+    int             sourcetype;         // Source: 0 - none, 1 - keyboard
+    int             sourcecd;           // Source code: keyboard - SDLK_Xxx
+    int             resulttype;         // Result: 0 - none, 1 - UKNC keyboard, 3 - command
+    unsigned int    resultcd;           // Result code: keyboard - key scan, command - ID
+};
+
+static KeyMappingStruct g_KeyMapping[] = {
+    { EVKEY, SDLK_TAB,      EVKEY,  0026 },
+    { EVKEY, SDLK_RETURN,   EVKEY,  0153 },
+    { EVKEY, SDLK_SPACE,    EVKEY,  0113 },
+    { EVKEY, SDLK_DOWN,     EVKEY,  0134 },
+    { EVKEY, SDLK_UP,       EVKEY,  0154 },
+    { EVKEY, SDLK_LEFT,     EVKEY,  0116 },
+    { EVKEY, SDLK_RIGHT,    EVKEY,  0133 },
+    { EVKEY, SDLK_F1,       EVKEY,  0010 },
+    { EVKEY, SDLK_F2,       EVKEY,  0011 },
+    { EVKEY, SDLK_F3,       EVKEY,  0012 },
+    { EVKEY, SDLK_F4,       EVKEY,  0014 },
+    { EVKEY, SDLK_F5,       EVKEY,  0015 },
+    { EVKEY, SDLK_F7,       EVKEY,  0152 },  // ÓÑÒ
+    { EVKEY, SDLK_F8,       EVKEY,  0151 },  // ÈÑÏ
+    { EVKEY, SDLK_F9,       EVKEY,  0171 },  // ÑÁÐÎÑ
+    { EVKEY, SDLK_1,        EVKEY,  0030 },
+    { EVKEY, SDLK_2,        EVKEY,  0031 },
+    { EVKEY, SDLK_3,        EVKEY,  0032 },
+    { EVKEY, SDLK_4,        EVKEY,  0013 },
+    { EVKEY, SDLK_5,        EVKEY,  0034 },
+    { EVKEY, SDLK_6,        EVKEY,  0035 },
+    { EVKEY, SDLK_7,        EVKEY,  0016 },
+    { EVKEY, SDLK_8,        EVKEY,  0017 },
+    { EVKEY, SDLK_9,        EVKEY,  0177 },
+    { EVKEY, SDLK_LSHIFT,   EVKEY,  0105 },
+    { EVKEY, SDLK_RSHIFT,   EVKEY,  0105 },
+};
+
+// Search for suitable mapping.
+//  sourcetype: 1 - keyboard, 2 - joystick
+//  ismenu: 0 - search for keyboard/joystick result; 1 - search for menu result
+KeyMappingStruct* FindKeyMapping(int sourcetype, int sourcecd, int ismenu)
+{
+    for (int i = 0; i < sizeof(g_KeyMapping) / sizeof(KeyMappingStruct); i++)
+    {
+        if (g_KeyMapping[i].sourcetype == sourcetype &&
+            g_KeyMapping[i].sourcecd == sourcecd)
+        {
+            if (!ismenu &&
+                (g_KeyMapping[i].resulttype == EVKEY || g_KeyMapping[i].resulttype == EVJOY))
+                return g_KeyMapping + i;
+            if (ismenu && g_KeyMapping[i].resulttype == EVCMD)
+                return g_KeyMapping + i;
+        }
+    }
+
+    return NULL;  // Mapping not found
+}
+
+void Main_OnKeyJoyEvent(SDL_Event evt)
+{
+    KeyMappingStruct* mapping;
+    int pressed = (evt.type == SDL_KEYDOWN || evt.type == SDL_JOYBUTTONDOWN);
+    int sourcetype = (evt.type == SDL_KEYDOWN || evt.type == SDL_KEYUP) ? EVKEY : EVJOY;
+
+    mapping = FindKeyMapping(sourcetype, evt.key.keysym.sym, FALSE);
+    if (mapping != NULL)  // UKNC event mapping found
+    {
+        BYTE result = mapping->resultcd;
+        Emulator_KeyboardEvent(result, pressed);
+    }
+    //else if (pressed)  // Commands works only on key/button press, not release
+    //{
+    //    mapping = FindKeyMapping(sourcetype, evt.key.keysym.sym, TRUE);
+    //    if (mapping != NULL)  // Command mapping found
+    //    {
+    //        Main_ExecuteCommand(mapping->resultcd);
+    //    }
+    //}
+}
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -65,33 +157,73 @@ void Main_DrawScreen()
 
 /////////////////////////////////////////////////////////////////////////////
 
-#if defined(_WIN32)
-#  undef main  //HACK for VC error LNK1561: entry point must be defined
-#endif
-int main(int argc, char* argv[])
+// Initialize the application
+bool InitInstance()
 {
+    // Parse INI file
+    Settings_ParseIniFile(g_ApplicationIniPath);
+
     // Init SDL video
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
-        return 255;  // Unable to initialize SDL
-
+        return false;  // Unable to initialize SDL
+    
 #ifdef _WIN32
-    SDL_putenv("SDL_VIDEODRIVER=windib");
-    SDL_putenv("SDL_DEBUG=1");
-    SDL_putenv("SDL_VIDEO_WINDOW_POS=300,200");
     SDL_WM_SetCaption("UKNCBTL SDL", "UKNCBTL SDL");
 #else
     SDL_ShowCursor(SDL_DISABLE);
 #endif
 
+    // Apply SDL settings from the "SDL" section of the INI file
+    // See http://www.libsdl.org/docs/html/sdlenvvars.html for the list of options
+    const TSETTINGSKEYVALUEMAP* pMapSdl = Settings_GetKeyValues(SETTINGS_SECTION_SDL);
+    if (pMapSdl != 0)
+    {
+        for (TSETTINGSKEYVALUEMAP::const_iterator itsdl = pMapSdl->begin(); itsdl != pMapSdl->end(); ++itsdl)
+        {
+            TCHAR buffer[256];
+            _sntprintf(buffer, 256, _T("%s=%s"), itsdl->first.c_str(), itsdl->second.c_str());
+            SDL_putenv(buffer);
+        }
+    }
+
     // Prepare screen surface
     g_Screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BITPERPIXEL, SDL_HWSURFACE | SDL_DOUBLEBUF);
     if (g_Screen == NULL)
-        return 254;  // Unable to set video mode
+        return false;  // Unable to set video mode
     g_UkncScreen = SDL_CreateRGBSurface(0, g_UkncScreenWid, g_UkncScreenHei, 32, 0,0,0,0);
     if (g_Screen == NULL)
-        return 253;
+        return false;
 
     if (!Emulator_Init())
+        return false;
+
+    return true;
+}
+
+void DoneInstance()
+{
+    Emulator_Done();
+
+    SDL_FreeSurface(g_Screen);
+    SDL_FreeSurface(g_UkncScreen);
+
+    SDL_Quit();
+}
+
+#if defined(_WIN32)
+#  undef main  //HACK for VC error LNK1561: entry point must be defined
+#endif
+int main(int argc, char* argv[])
+{
+    // Get application path
+    g_ApplicationExePath.append(argv[0]);
+
+    // Calculate INI file path
+    g_ApplicationIniPath = g_ApplicationExePath;
+    g_ApplicationIniPath.resize(g_ApplicationIniPath.find_last_of(_T('.')));
+    g_ApplicationIniPath.append(_T(".ini"));
+
+    if (! InitInstance())
         return 255;
 
     Emulator_Start();
@@ -116,7 +248,7 @@ int main(int argc, char* argv[])
                 if (evt.type == SDL_KEYDOWN || evt.type == SDL_KEYUP ||
                     evt.type == SDL_JOYBUTTONDOWN || evt.type == SDL_JOYBUTTONUP)
                 {
-                    //TODO: Main_OnKeyJoyEvent(evt);
+                    Main_OnKeyJoyEvent(evt);
                 }
             }
         }
@@ -150,12 +282,8 @@ int main(int argc, char* argv[])
     }
 
     Emulator_Stop();
-    Emulator_Done();
 
-    SDL_FreeSurface(g_Screen);
-    SDL_FreeSurface(g_UkncScreen);
-
-    SDL_Quit();
+    DoneInstance();
 
     return 0;
 }
